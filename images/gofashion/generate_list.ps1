652df13b2ed38adbc63df7f6355185f50c7d5f21
Marcelo <devfullmarcelo@gmail.com>
2025-12-30 05:29:15 -0300

    att

--- images/gofashion/generate_list.ps1
+++
@@ -1,7 +0,0 @@
-﻿$folder = 'c:\Users\Windows\Desktop\DevMarcelo\images\gofashion'
-$files = Get-ChildItem -Path $folder -File -Include *.jpg, *.jpeg, *.png, *.gif, *.webp | Sort-Object Name
-$files | ForEach-Object {
-  $name = $_.Name
-  $title = $_.BaseName
-  Write-Output "      { name: '$name', title: '$title' },"
-}
